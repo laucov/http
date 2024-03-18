de34ef1aeafbd24b5967f802f95ef75bbd4e6f18
covaleski <covaleski23@gmail.com>
2024-03-18 10:48:07 -0300

    feat: create new `Route`

---
+++ src/Routing/Call/Route.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * This file is part of Laucov's HTTP Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package http
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+namespace Laucov\Http\Routing\Call;
+
+use Laucov\Http\Message\OutgoingResponse;
+use Laucov\Http\Message\ResponseInterface;
+
+/**
+ * Stores information about a processed route callback.
+ */
+class Route
+{
+    /**
+     * Create the route instance.
+     */
+    public function __construct(
+        /**
+         * Callback object.
+         */
+        protected Callback $callback,
+
+        /**
+         * Callback parameters.
+         */
+        protected array $parameters,
+
+        /**
+         * Route preludes.
+         * 
+         * @var array<Prelude>
+         */
+        protected array $preludes,
+    ) {
+    }
+
+    /**
+     * Run the route procedures.
+     */
+    public function run(): ResponseInterface
+    {
+        // Run callback.
+        $callback = $this->callback->callback;
+        if (is_callable($callback)) {
+            // Execute callable.
+            $result = $callback(...$this->parameters);
+        } else {
+            // Instantiate class and call method.
+            [$class_name, $method_name] = $callback;
+            $instance = new $class_name(...$this->callback->constructorArgs);
+            $result = $instance->{$method_name}(...$this->parameters);
+        }
+
+        // Process result.
+        return $result instanceof ResponseInterface
+            ? $result
+            : $this->createResponse($result);
+    }
+
+    /**
+     * Handle unknown output and return it as a `ResponseInterface` object.
+     */
+    protected function createResponse(mixed $content): ResponseInterface
+    {
+        // Handle string or stringable object.
+        if (is_string($content) || $content instanceof \Stringable) {
+            // Create response from string.
+            $response = new OutgoingResponse();
+            return $response->setBody((string) $content);
+        }
+
+        // Fail if received other type of content.
+        $message = 'Received an unexpected result from a route closure.';
+        throw new \RuntimeException($message);
+    }
+}
---
+++ tests/Routing/Call/RouteTest.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ * This file is part of Laucov's HTTP Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package http
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Routing\Call;
+
+use Laucov\Http\Message\IncomingResponse;
+use Laucov\Http\Message\ResponseInterface;
+use Laucov\Http\Routing\Call\Callback;
+use Laucov\Http\Routing\Call\Route;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Laucov\Http\Routing\Call\Route
+ */
+class RouteTest extends TestCase
+{
+    public function callbackProvider(): array
+    {
+        // Create callbacks.
+        $callbacks = [
+            new Callback(
+                fn ($name) => "Hello, {$name}!",
+                null,
+                [],
+            ),
+            new Callback(
+                fn ($x, $y) => new IncomingResponse('x + y = ' . ($x + $y)),
+                null,
+                [],
+            ),
+            new Callback([B::class, 'y'], [2, 1], []),
+            new Callback([B::class, 'y'], [3, 5], []),
+        ];
+
+        return [
+            [[$callbacks[0], ['John'], []], 'Hello, John!'],
+            [[$callbacks[1], [5, 8], []], 'x + y = 13'],
+            [[$callbacks[2], [5], []], '11'],
+            [[$callbacks[2], [3], []], '7'],
+            [[$callbacks[3], [5], []], '20'],
+            [[$callbacks[3], [3], []], '14'],
+            // @todo Test with preludes.
+        ];
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::createResponse
+     * @covers ::run
+     * @uses Laucov\Http\Message\AbstractIncomingMessage::__construct
+     * @uses Laucov\Http\Message\AbstractMessage::getBody
+     * @uses Laucov\Http\Message\AbstractOutgoingMessage::setBody
+     * @uses Laucov\Http\Message\IncomingResponse::__construct
+     * @dataProvider callbackProvider
+     */
+    public function testCanSetupAndRun(array $args, string $expected): void
+    {
+        $route = new Route(...$args);
+        $response = $route->run();
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $content = (string) $response->getBody();
+        $this->assertSame($expected, $content);
+    }
+
+    /**
+     * @covers ::createResponse
+     * @uses Laucov\Http\Routing\Call\Callback::__construct
+     * @uses Laucov\Http\Routing\Call\Route::__construct
+     * @uses Laucov\Http\Routing\Call\Route::run
+     */
+    public function testCallbackMustReturnValidOutput(): void
+    {
+        // The output must be a string, a stringable object or a response.
+        $this->expectException(\RuntimeException::class);
+        $closure = fn () => ['foo', 'bar'];
+        $callback = new Callback($closure, null, []);
+        $route = new Route($callback, [], []);
+        $route->run();
+    }
+}
+
+class B
+{
+    public function __construct(
+        protected int $a,
+        protected int $b,
+    ) {
+    }
+
+    public function y(int $x): string
+    {
+        return (string) ($this->a * $x + $this->b);
+    }
+}
